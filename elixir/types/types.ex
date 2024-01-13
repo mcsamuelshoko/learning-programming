645681f9421a5747ff65d6438d875823f18cf50e
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-01-13 18:24:45 +0200

    update types.ex - added more types - some of them had been left out then realized it on checking the cheatsheet doc

--- elixir/types/types.ex
+++ elixir/types/types.ex
@@ -14,6 +14,9 @@ IO.puts(char)
 b = "hello"
 # string concatenation
 IO.puts(b <> " e-l-i-x-i-r !")
+# string interpolation
+IO.puts("Its good to greet with a '#{b}'. ")
+
 
 # floats
 c = 0.5578
@@ -35,10 +38,31 @@ e = """
 """
 # IO.puts(e)
 
-#lists or enums
+# lists or enums
 l = [1,"hej!", 0x3fa, :thats_an_atom]
 #  IO.puts(l)
 
-#tuples
+
+# tuples
 t = {:err, "couldn't find limit"}
 # IO.puts(t)
+
+
+# keyword list (can duplicate keys)
+k = [code: 200, status: "ok", status: "done", message: "success"]
+# IO.puts(k)
+
+
+# maps (no duplicate keys)
+m = %{a => 404, b => "error"}
+# IO.puts(m)
+
+
+# binary
+bin = <<100,154, 165>>
+# IO.puts(bin)
+
+# range
+
+r = 1..a
+# IO.puts(r)
