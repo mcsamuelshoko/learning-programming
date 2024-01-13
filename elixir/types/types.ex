f35cb4fed48178bdc33f8cfd0b30b8331049957c
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-01-13 17:15:12 +0200

    feat: create types.ex - turns out i had forgot to about the items i had learned - although it had been a  few days past but still its the fundamental stuff so how and why - wrote the stuff down and had some errors along the way,.. need to learn about string formatting i guess.

---
+++ elixir/types/types.ex
@@ -0,0 +1,43 @@
+# types in elixir
+
+# [ elixir uses data types & data structures, and no objects 🙅‍♂️ ]
+
+# numbers
+a = 1 + 2
+IO.puts(a)
+
+# chars
+char = ~c"c"
+IO.puts(char)
+
+# strings
+b = "hello"
+IO.puts(b <> " e-l-i-x-i-r !")
+
+# floats
+c = 0.5578
+IO.puts(c)
+
+# boolean
+d = false
+IO.puts(d)
+
+# atoms
+:an_atom
+IO.puts(:an_atom)
+
+# multiline string
+e = """
+####################################
+##      CONTAINER TYPES          ###
+####################################
+"""
+# IO.puts(e)
+
+#lists or enums
+l = [1,"hej!", 0x3fa, :thats_an_atom]
+#  IO.puts(l)
+
+#tuples
+t = {:err, "couldn't find limit"}
+# IO.puts(t)
