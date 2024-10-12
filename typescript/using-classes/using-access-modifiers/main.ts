085e1e6c8eeb29943d9a5b02f5c7b49ec59d2be2
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-10-12 21:06:22 +0200

    update main.ts

--- typescript/using-classes/using-access-modifiers/main.ts
+++ typescript/using-classes/using-access-modifiers/main.ts
@@ -2,6 +2,26 @@ export function add(a: number, b: number): number {
   return a + b;
 }
 
+export class Product {
+  private name;
+  private price;
+
+  constructor(name: string, price: number) {
+    this.name = name;
+    this.price = price;
+  }
+
+  copy(name: string) {
+    return new Product(name, this.price);
+  }
+
+  equal(p1: Product, p2: Product) {
+    return p1.name == p2.name && p1.price == p2.price;
+  }
+}
+
+let table1:Product = Product('table',15);
+
 // Learn more at https://deno.land/manual/examples/module_metadata#concepts
 if (import.meta.main) {
   console.log("Add 2 + 3 =", add(2, 3));
