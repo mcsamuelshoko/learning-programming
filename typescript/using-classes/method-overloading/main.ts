5d9f531094b17eca102b614a00df5b69c2804688
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-10-13 23:59:33 +0200

    Mirrored code from sandbox (copied) <https://codesandbox.io/s/method-overloading-b1dqi>  -update main.ts

--- typescript/using-classes/method-overloading/main.ts
+++ typescript/using-classes/method-overloading/main.ts
@@ -1,7 +1,18 @@
-export class Product {
-  constructor(private name: string, private price: number) { }
-  
+class Product {
+  constructor(public name: string, public price: number) { }
 }
+
+class Products {
+  private products: Product[] = [];
+  add(product: Product) {
+    this.products.push(product);
+  }
+}
+
+const products = new Products();
+products.add(new Product("Table", 400));
+products.add(new Product("Chair", 100));
+products.add(new Product("Lamp", 30));
 
 
 let table1 = new Product('myTable', 15);
