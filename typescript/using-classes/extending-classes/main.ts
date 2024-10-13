f71371fac51cbb4bc59faf63d9947fdd545e0296
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-10-14 01:54:03 +0200

    Finished on 'Extended-classes' update main.ts

--- typescript/using-classes/extending-classes/main.ts
+++ typescript/using-classes/extending-classes/main.ts
@@ -1,6 +1,21 @@
-export class Product {
-  constructor(public name: string, public price: number) {}
+abstract class Product {
+  constructor(public name: string, public price: number) { }
   log() {
     console.log(this.name, this.price);
   }
 }
+
+class Table extends Product {
+  constructor(public name: string, public price: number, public legs: number) {
+      super(name,price);
+  }
+
+  log(){
+    console.log(this.name, this.price, this.legs);
+  }
+}
+
+const table = new Table("Table", 400, 4);
+table.log();
+
+// const product = new Product("Chair",100);
