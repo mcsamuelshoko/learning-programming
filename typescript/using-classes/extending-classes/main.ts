2b7e1ce7f79b948e35ace0f90c32d572f5793761
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-10-14 01:34:27 +0200

    update main.ts - mirrored code snippet in <https://codesandbox.io/s/exending-classes-qlhfh>

--- typescript/using-classes/extending-classes/main.ts
+++ typescript/using-classes/extending-classes/main.ts
@@ -1,8 +1,6 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
-
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+export class Product {
+  constructor(public name: string, public price: number) {}
+  log() {
+    console.log(this.name, this.price);
+  }
 }
