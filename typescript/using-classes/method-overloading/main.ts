7510956ff944e94f39927da5de9739ffcf48c628
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-10-14 00:59:00 +0200

    Finished on 'method-overloading' - update main.ts

--- typescript/using-classes/method-overloading/main.ts
+++ typescript/using-classes/method-overloading/main.ts
@@ -3,10 +3,25 @@ class Product {
 }
 
 class Products {
+
   private products: Product[] = [];
+
   add(product: Product) {
     this.products.push(product);
   }
+
+  filter(name: string): Product[]
+  filter(name: string, price: number): Product[]
+  filter(name: string, price?: number): Product[] {
+    if (price === undefined) {
+      return this.products.filter((product) => product.name === name);
+    }
+    else {
+      return this.products.filter((product) => product.name === name && product.price === price);
+    }
+  }
+
+
 }
 
 const products = new Products();
@@ -15,5 +30,10 @@ products.add(new Product("Chair", 100));
 products.add(new Product("Lamp", 30));
 
 
-let table1 = new Product('myTable', 15);
-console.log(table1);
+let table1 = new Product('Table', 400);
+products.add(table1);
+
+// console.log(table1);
+
+// console.log('[i]checking for item(s): ', products.filter(table1.name, table1.price))
+console.log('[🔍] Product search results: ', products.filter('Chair',100));
