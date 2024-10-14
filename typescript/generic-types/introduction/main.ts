2e704d73f3069803d49e52675cd5fbd678678e76
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-10-14 22:20:08 +0200

    update main.ts - finished intro

--- typescript/generic-types/introduction/main.ts
+++ typescript/generic-types/introduction/main.ts
@@ -1,8 +1,8 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+/**
+ * 
+ * https://learntypescript.dev/06/intro
+ */
 
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+function firstOrNull(array: any[]): any[] | null {
+  return array.length === 0 ? null : array[0];
 }
