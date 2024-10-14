0b3d037468c549e108d2b4c5e51d2f16a94cd945
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-10-14 23:09:15 +0200

    Finished subtopic - update main.ts *code is intentionally left broken

--- typescript/generic-types/using-some-standard-generic-types/main.ts
+++ typescript/generic-types/using-some-standard-generic-types/main.ts
@@ -1,1 +1,50 @@
+// Understanding the generic type syntax
 type Coordinate = [number, number];
+
+
+// Array<T>
+let coordinates: Array<Coordinate>;
+coordinates = [
+  [11, 45],
+  [33, 86]
+];
+
+// Promise<T>
+const promisedResponse: Promise<Response> = fetch("https://swapi.dev/api/");
+promisedResponse.then((res) => console.log(res.ok));
+
+// Record<K,V>
+
+type Result = {
+  firstName: string;
+  surname: string;
+  score: number;
+
+};
+
+type ResultRecord = Record<'alexm' | 'elig' | 'jamh', Result>;
+
+const results: ResultRecord = {
+  'alexm': {
+    firstName: 'alex',
+    surname: 'mercer',
+    score: 80
+  },
+  'elig': {
+    firstName: 'elizabeth',
+    surname: 'greene',
+    score: 90
+  },
+  'jamh': {
+    firstName: 'james',
+    surname: 'heller',
+    score: 75
+  },
+  'revb': {
+    firstName: 'reverend',
+    surname: 'bob',
+    score: 60
+  }
+}
+
+console.log(results);
