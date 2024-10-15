4c76c61362bc50cb670967f095d5a87790c3bc64
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-10-15 21:45:47 +0200

    started subtopic: 'Creating generic type aliases' - create deno.json, main.ts and main_test.ts

---
+++ typescript/generic-types/creating-generic-type-aliases/main.ts
@@ -0,0 +1,33 @@
+// Generic type alias syntax
+
+type typeName<T1, T2> = {
+  //...
+}
+
+// Generic type alias example
+
+// TODO - create the Form generic type
+
+interface Form<T> {
+  errors: {
+    [K in keyof T]?: string;
+  },
+  values: T
+}
+
+interface Contact {
+  name: string;
+  email: string;
+}
+
+const contactForm: Form<Contact> = {
+  errors: {
+    email: "This must be a valid email address"
+  },
+  values: {
+    name: "Bob",
+    email: "bob@someemail.com"
+  }
+};
+
+console.log(contactForm);
