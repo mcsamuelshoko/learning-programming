fc6231fdaf1a13444219cb2d510f5eea62a63cd2
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-10-17 06:32:11 +0200

    update main.ts - finished subtopic

--- typescript/generic-types/implementing-generic-parameter-defaults/main.ts
+++ typescript/generic-types/implementing-generic-parameter-defaults/main.ts
@@ -9,12 +9,27 @@
 
 // An example
 
-function firstOrNull<T>(array: T[]): T | null {
+function firstOrNull<T = string>(array: T[]): T | null {
   return array.length === 0 ? null : array[0];
 }
 
-interface Component<T1, T2> {
+interface Component<T1 = string, T2 = any> {
   name: T1;
   props: T2;
   log: () => void;
 }
+
+
+const button: Component = {
+  name: 'button',
+  props: {
+    text: 'save',
+  },
+  log: () => console.log("save button")
+}
+
+console.log(button.props.text);
+console.log(button.props.text2)
+
+const first = firstOrNull([1, 2, 3])
+console.log(first);
