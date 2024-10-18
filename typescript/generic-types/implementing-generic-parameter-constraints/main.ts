9fdde5ba0b07253a62ca9f1f702209d42d62eea6
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-10-18 23:19:32 +0200

    update main.ts - finished subtopic

--- typescript/generic-types/implementing-generic-parameter-constraints/main.ts
+++ typescript/generic-types/implementing-generic-parameter-constraints/main.ts
@@ -13,6 +13,47 @@
 interface Logable {
   log: () => void;
 }
-function logItems<T>(items: T[]): void {
+
+function logItems<T extends Logable>(items: T[]): void {
   items.forEach(item => item.log());
 }
+
+const heading = {
+  name: "Heading",
+  props: {
+    text: 'Chapter 1'
+  },
+  log: () => console.log('Chapter 1 Heading'),
+};
+const button = {
+  name: "Button",
+  props:{
+    text: "Save",
+  },
+  log: () => console.log('Save button')
+};
+
+logItems([heading, button])
+
+// A more complex example
+
+interface Form<T>{
+  values: T,
+}
+
+function getFieldValue<T, K extends keyof T>(form: Form<T>, fieldName:K){
+  return form.values[fieldName];
+}
+
+
+const contactForm = {
+  values:{
+    name:'Bob',
+    email:"bob@someemail.com",
+  },
+};
+
+console.log(getFieldValue(contactForm,"name"));
+// console.log(getFieldValue(contactForm,"phone")); // 2nd parameter must be a key-name in first parameter
+
+
