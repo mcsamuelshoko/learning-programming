d9f72a4fb8d19a92e6226bcc599f9941c8181606
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-10-19 02:45:24 +0200

    finished subtopic - update main.ts

--- typescript/generic-types/Using-generic-rest-elements-with-tuple-types/main.ts
+++ typescript/generic-types/Using-generic-rest-elements-with-tuple-types/main.ts
@@ -5,3 +5,26 @@ type Scores = [string, ...number[]];
 
 // Using generic rest element types
 
+type nameAndThings<T extends unknown[]> = [name: string, ...things: T];
+
+let bobScores: nameAndThings<number[]>;
+
+bobScores = ["Bob", 4, 9, 3];
+
+
+let billGrades: nameAndThings<('A' | 'B' | 'C')[]>;
+
+billGrades = ["Bill", 'A', 'A', "C"];
+
+
+// Using generic rest element types in functions
+
+function logThings<T extends unknown[]>(name: string, ...things: T) {
+  console.log(things);
+}
+
+logThings('bob', 1,4,6);
+
+logThings('Bob',4, '9', 3);
+
+// logThings<number[]>('Bob', 4, '9', 3); //* broken intentionally
