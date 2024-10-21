2737d2ace63fb4c8ff78d5fe86a76a1d903e27d8
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-10-21 23:55:34 +0200

    update main.ts, - duplicated the function for clarity per stage

--- typescript/generic-types/spreading-generic-tuple-parameters/main.ts
+++ typescript/generic-types/spreading-generic-tuple-parameters/main.ts
@@ -1,6 +1,33 @@
 // An example function that uses the spread operator
 
-function merge(names: string[], scores: number[]) {
+function _merge(names: string[], scores: number[]) {
   return [...names, ...scores];
 }
 
+let _scores = _merge(['Bill', 'Jane'], [8, 9])
+
+
+// Spreading generic tuple parameters to narrow the return type
+
+function __merge<Names extends string[], Scores extends number[]>(
+  names: Names,
+  scores: Scores
+) {
+  return [...names, ...scores];
+}
+
+function ___merge<Names extends string[], Scores extends number[]>(
+  names: [...Names],
+  scores: [...Scores]
+) {
+  return [...names, ...scores]
+}
+
+function merge<Names extends string[], Scores extends number[]>(
+  names: [...Names],
+  scores: [...Scores],
+): [...Names, ...Scores] {
+  return [...names, ...scores];
+}
+
+let scores = merge(["Bill", "Jane"], [8, 9]);
