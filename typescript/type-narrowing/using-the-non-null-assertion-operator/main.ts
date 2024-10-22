e001c7b39b4a05978027369ec4e692ac6ef8c554
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-10-22 06:19:15 +0200

    Started subtopic 'Using the non-null assertion operator' - create deno.json, main.ts and main_test.ts

---
+++ typescript/type-narrowing/using-the-non-null-assertion-operator/main.ts
@@ -0,0 +1,14 @@
+// Understanding use cases for the non-null assertion operator
+
+function duplicate(text: string | null) {
+  let fixString = function() {
+    if (text === null || text === undefined) {
+      text = "";
+    }
+  };
+  fixString();
+
+  return text.concat(text);
+}
+
+console.log(duplicate("hello"));
