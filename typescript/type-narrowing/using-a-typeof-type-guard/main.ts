e9f1b504ce865b26df258f37904b2af70a0ea9f1
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-10-29 00:31:22 +0200

    finished subtopic - fixed filename mismatch - update main.ts and main_test.ts

--- typescript/type-narrowing/using-a-typeof-type-guard/main.ts
+++ typescript/type-narrowing/using-a-typeof-type-guard/main.ts
@@ -1,8 +1,25 @@
-export function add(a: number, b: number): number {
-  return a + b;
+/**
+ * A type guard is a mechanism of narrowing a type. 
+ * There are several types of type guards in TypeScript, 
+ * and we will learn about the typeof method in this lesson.
+ * 
+ */
+
+// Understanding the `typeof` type guard
+
+function double(item: string | number) {
+  // TODO:
+  // return `item.concat(item)` if item is a string
+  if (typeof item === 'string') {
+    return item.concat(item);
+  }
+  // return `item + item` if item is a number
+  else {
+    return item + item;
+  }
 }
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+console.log(double("hello"));
+console.log(double(1));
+
+
--- typescript/type-narrowing/using-a-typeof-type-guard/main_test.ts
+++ typescript/type-narrowing/using-a-typeof-type-guard/main_test.ts
@@ -1,25 +1,6 @@
-/**
- * A type guard is a mechanism of narrowing a type. 
- * There are several types of type guards in TypeScript, 
- * and we will learn about the typeof method in this lesson.
- * 
- */
+import { assertEquals } from "@std/assert";
+import { add } from "./main.ts";
 
-// Understanding the `typeof` type guard
-
-function double(item: string | number) {
-  // TODO:
-  // return `item.concat(item)` if item is a string
-  if (typeof item === 'string') {
-    return item.concat(item);
-  }
-  // return `item + item` if item is a number
-  else {
-    return item + item;
-  }
-}
-
-console.log(double("hello"));
-console.log(double(1));
-
-
+Deno.test(function addTest() {
+  assertEquals(add(2, 3), 5);
+});
