63e0333d7080ede896e54928c326c27d50c7099c
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-11-06 22:03:28 +0200

    update my_functions.exs - did shorthand functions

--- elixir/elixir-succinctly/modules-and-functions/my_functions.exs
+++ elixir/elixir-succinctly/modules-and-functions/my_functions.exs
@@ -7,4 +7,16 @@ defmodule MyFunctions do
   def sub(a, b) do
     a - b
   end
+
+
 end
+
+# def print_result(f) do
+#   IO.puts(f())
+# end
+
+def multi(a, b), do: a * b
+
+IO.puts(MyFunctions.sub(4, 7))
+IO.puts(MyFunctions.sum(5, 8))
+IO.puts(multi(8,4))
