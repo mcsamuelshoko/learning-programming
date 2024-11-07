c53f96e8265a5f6f6fa4bf2eb669353690034e8f
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-11-07 22:49:39 +0200

    update my_functions.exs - oof

--- elixir/elixir-succinctly/modules-and-functions/my_functions.exs
+++ elixir/elixir-succinctly/modules-and-functions/my_functions.exs
@@ -8,15 +8,19 @@ defmodule MyFunctions do
     a - b
   end
 
-
+  def multi(a, b), do: a * b
 end
 
-# def print_result(f) do
-#   IO.puts(f())
-# end
-
-def multi(a, b), do: a * b
+defmodule DeezOof do
+  def print_result(f) do
+    IO.puts(f())
+  end
+end
 
 IO.puts(MyFunctions.sub(4, 7))
 IO.puts(MyFunctions.sum(5, 8))
-IO.puts(multi(8,4))
+IO.puts(MyFunctions.multi(8, 4))
+
+a = fn -> 42 end
+
+# print_result(a)
