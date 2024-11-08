7259ce74ee460edee448752ed28bdad7e5d6ecb5
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-11-08 23:23:58 +0200

    update pattern_match.exs - pattern matching functions

--- elixir/elixir-succinctly/modules-and-functions/pattern_match.exs
+++ elixir/elixir-succinctly/modules-and-functions/pattern_match.exs
@@ -1,1 +1,19 @@
+# Pattern Matching
 
+a = 3
+# ^a = 4 #! error
+
+# Pattern matching in functions
+
+defmodule Bot do
+    def greet("") do
+        IO.puts("No name to greet")
+    end
+
+    def greet(name) do
+        IO.puts("Hello #{name}")
+    end
+end
+
+Bot.greet("")
+Bot.greet("mom")
