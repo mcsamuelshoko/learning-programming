305a3f2e2d2679e505061924ae0af8e06e1d1562
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-11-10 22:46:42 +0200

    update recursion.exs - did some recursion tail-call optimization,.. good stuff

--- elixir/elixir-succinctly/modules-and-functions/recursion.exs
+++ elixir/elixir-succinctly/modules-and-functions/recursion.exs
@@ -4,6 +4,29 @@ defmodule Factorial do
   end
 
   def do_it(n) do
-    n*do_it(n - 1)
+    n * do_it(n - 1)
   end
 end
+
+IO.puts(Factorial.do_it(100))
+
+# Tail-call Optimization
+
+defmodule TailCallFactorial do
+  def do_it(n) do
+    internal_do_it(n, 1)
+  end
+
+  defp internal_do_it(0, acc) do
+    acc
+  end
+
+  defp internal_do_it(n, acc) do
+    internal_do_it(n - 1, acc * n)
+  end
+end
+
+IO.puts "Starting Tail-call Optimized"
+
+IO.puts(TailCallFactorial.do_it(100))
+
