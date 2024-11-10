82605d769d358bdea2f4b48969e108f2436cdd01
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-11-11 00:42:05 +0200

    build(deps): create go.mod and main.go - started topic on "Let's Go",... getting started

---
+++ go/go-succinctly/lets-go/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("hi mom, good morning to my family")
+}
+
+// func factorialBomb(n:number){
+// 	return n * factorialBomb(n-1)
+// }
