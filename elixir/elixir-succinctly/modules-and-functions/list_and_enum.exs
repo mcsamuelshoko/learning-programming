644743f4bba6063f073172b859dcb0e15808233a
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-11-12 00:37:17 +0200

    update list_and_enum.exs - finished topic & i loved it

--- elixir/elixir-succinctly/modules-and-functions/list_and_enum.exs
+++ elixir/elixir-succinctly/modules-and-functions/list_and_enum.exs
@@ -1,26 +1,41 @@
 defmodule ListUtils do
-    def sum([]) do
-        0
-    end
+  def sum([]) do
+    0
+  end
 
-    def sum([h|t]) do
-        h + sum(t)
-    end
+  def sum([h | t]) do
+    h + sum(t)
+  end
 end
 
-IO.puts ListUtils.sum([1,4,5,6,7,10])
+IO.puts(ListUtils.sum([1, 4, 5, 6, 7, 10]))
 
 defmodule MapMod do
-
 end
 
-book = % {
-    title: "Programming Elixir",
-    author: %{
-        first_name: "Dave",
-        last_name: "Thomas"
-    },
-    year: 2018
+book = %{
+  title: "Programming Elixir",
+  author: %{
+    first_name: "Dave",
+    last_name: "Thomas"
+  },
+  year: 2018
 }
 
-IO.puts book[:title]
+IO.puts(book[:title])
+
+# trying to change an immutable value
+#! book[:title] = "Programming Java"
+
+# proper way
+book1 = Map.put(book, :title, "Programming Elixir >=1.6")
+IO.puts(book1[:title])
+
+new_book = %{
+  book| title: "Programming Elixir >=1.7"
+}
+
+IO.puts new_book[:title]
+
+{:ok,x} = Map.fetch(book,:year)
+IO.puts "The book year is: #{x}"
