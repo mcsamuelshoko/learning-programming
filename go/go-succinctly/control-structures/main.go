c4a5633127e5dc5ef0a1997ef2fff59f2e01fbd6
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-11-15 04:28:08 +0200

    finished subtopic on loops - update main.go

--- go/go-succinctly/control-structures/main.go
+++ go/go-succinctly/control-structures/main.go
@@ -16,10 +16,18 @@ func main() {
 
 	fmt.Printf("[INPUT]%s %s %s %s\n", val1, val2, val3, val4)
 
-	fmt.Printf("%s is %s\n", os.Args[1], oddOrEven(os.Args[1]))
-	fmt.Printf("%s is %s\n", os.Args[2], oddOrEven(os.Args[2]))
-	fmt.Printf("%s is %s\n", os.Args[3], oddOrEven(os.Args[3]))
-	fmt.Printf("%s is %s\n", os.Args[4], oddOrEven(os.Args[4]))
+	{
+		fmt.Printf("%s is %s\n", os.Args[1], oddOrEven(os.Args[1]))
+		fmt.Printf("%s is %s\n", os.Args[2], oddOrEven(os.Args[2]))
+		fmt.Printf("%s is %s\n", os.Args[3], oddOrEven(os.Args[3]))
+		fmt.Printf("%s is %s\n", os.Args[4], oddOrEven(os.Args[4]))
+	}
+
+	// Looping with the `for` statement
+	for i := 1; i < len(os.Args); i++ {
+		fmt.Printf("%s is %s\n", os.Args[i], oddOrEven(os.Args[i]))
+	}
+
 }
 
 func oddOrEven(value string) string {
