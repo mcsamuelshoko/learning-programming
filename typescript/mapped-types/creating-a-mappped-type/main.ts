4e8c6b3446259c9a5ab9d5077c1d67afd855935e
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-11-17 20:13:29 +0200

    finished subtopic - update main.ts

--- typescript/mapped-types/creating-a-mappped-type/main.ts
+++ typescript/mapped-types/creating-a-mappped-type/main.ts
@@ -15,3 +15,31 @@
 * This creates a new type containing the keys from 
 * another type.
 */
+
+//* An example
+
+interface Form<T> {
+  values: T;
+  errors: {[K in keyof T]?:string};
+}
+
+const contactForm: Form<{ name: string; email: string, mobile: string }> = {
+  values: {
+    name: "Bob",
+    email: "bob@someemail.com",
+    mobile: "0793 869487368"
+  },
+  errors: {
+    email: "Invalid email address",
+    mobile:"You must add a mobile number"
+  }
+};
+console.log(contactForm);
+
+/**
+ ** Summary
+ *
+ * Mapped types allow us to create new types based on an 
+ * existing type. They are useful for handling generic 
+ * data in a strongly-typed manner. 
+ */
