96cf80d209f7f4e59e4c3c31259fc1aa7c214151
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-11-23 21:15:45 +0200

    update main.go - that's enough for today tyJ🙏

--- go/go-succinctly/concurrency/main.go
+++ go/go-succinctly/concurrency/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"strconv"
 	"time"
 )
@@ -13,7 +14,67 @@ func message(s string) {
 	}
 }
 
+func broadcast(c chan int) {
+	// infinite loop to create rndom numbers
+	for {
+		/**
+		* generate a random number 0 - 999
+		* and put it into the channel
+		 */
+
+		c <- rand.Intn(999)
+	}
+}
+
+func generateAccountNumber(accountNumberChannel chan int) {
+	// interval variable to store last generated account number
+	var accountNumber int
+	accountNumber = 30000001
+
+	for {
+		accountNumberChannel <- accountNumber
+		// increment the account number by 1
+		accountNumber += 1
+	}
+}
+
 func main() {
-	go message("goroutine")
-	message("normal message")
+	// Routines
+	if false {
+		go message("goroutine")
+		message("normal message")
+	}
+
+	// Channels
+	{
+		if false {
+			numbersStation := make(chan int)
+			// execute broadcast in a seperate thread
+			go broadcast(numbersStation)
+			// retrieve values from the channel
+			for num := range numbersStation {
+				// delay for artistic effect only
+				time.Sleep(1000 * time.Millisecond)
+
+				fmt.Printf("%d ", num)
+			}
+		}
+
+		// Better example
+		{
+			accountNumberChannel := make(chan int)
+
+			// start the goroutine that generates account numbers
+			go generateAccountNumber(accountNumberChannel)
+
+			fmt.Printf("SMITH: %d\n", <-accountNumberChannel)
+			fmt.Printf("SINGH: %d\n", <-accountNumberChannel)
+			fmt.Printf("JONES: %d\n", <-accountNumberChannel)
+			fmt.Printf("LOPEZ: %d\n", <-accountNumberChannel)
+			fmt.Printf("CLARK: %d\n", <-accountNumberChannel)
+
+		}
+
+	}
+
 }
