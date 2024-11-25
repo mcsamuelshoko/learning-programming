7f5b7b564b5ed9f6651eb0c90a74aae4a12cb0fd
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-11-26 01:19:32 +0200

    Started subtopic "Using `Object.freeze`" - create deno.json, main.ts and main_test.ts

---
+++ typescript/immutable-types/using-object.freeze/main.ts
@@ -0,0 +1,7 @@
+//* Using `Object.freeze` on arrays
+
+//? Object.freeze is a JavaScript method 
+//? that can make objects and arrays immutable at runtime
+
+let billScores = Object.freeze([90, 65, 80]);
+
