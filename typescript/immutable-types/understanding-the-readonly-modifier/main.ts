5b87788b0d585d13b667b5d715dfaf6a6265cb48
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-11-25 07:31:26 +0200

    update main.ts - finished subtopic

--- typescript/immutable-types/understanding-the-readonly-modifier/main.ts
+++ typescript/immutable-types/understanding-the-readonly-modifier/main.ts
@@ -22,9 +22,42 @@
 // An example
 type Person = {
   name: string;
-  age: number;
+  readonly age: number;
 };
+
 const bob: Person = {
   name: "Bob",
   age: 30
 };
+
+bob.age = 31
+
+interface Animal {
+  name: string;
+  readonly age: number;
+};
+
+const barry: Animal = {
+  name: "Barry",
+  age: 4
+};
+
+barry.age = 5
+
+class Vehicle {
+  constructor(name: string, readonly topSpeed: number) {
+  }
+}
+const mini = new Vehicle("Mini", 120);
+mini.topSpeed = 125;
+
+console.log(bob.age, barry.age, mini.topSpeed)
+
+/**
+ * Summary
+ * 
+ * The readonly modifier allows immutable primitive 
+ * properties to be created. However, this is only 
+ * compile-time immutability and not runtime immutability.
+ * 
+ */
