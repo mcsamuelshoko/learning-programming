b51176311df6ec3bfc4f85fa4c2bf461f923fb28
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-11-25 11:00:45 +0200

    update main.go - finished `channels` subtopic

--- go/go-succinctly/concurrency/main.go
+++ go/go-succinctly/concurrency/main.go
@@ -110,6 +110,113 @@ func main() {
 			}
 		}
 
+		// Buffered Channels
+		{
+			//? mychannel := make(chan chanType, capacity)
+
+			if false {
+				// un-buffered channel
+				c := make(chan int)
+				c <- 3 //! error
+				fmt.Println("OK.")
+			}
+
+			{
+				// buffered channel, channel capacity 3
+				c := make(chan int, 3)
+				c <- 3
+				c <- 4
+				c <- 5
+				// c <- 6
+				fmt.Println("OK.")
+			}
+
+			// communicating on multiple channels
+			if false {
+				broadcast2 := func(nsChannel chan int, cChannel chan bool) {
+					numbers := []int{
+						101,
+						102,
+						103,
+						104,
+						105,
+						106,
+						107,
+						108,
+						109,
+						110,
+					}
+					i := 0
+
+					for {
+						// see the which channel has items
+
+						select {
+						/**
+						* if numbersChannel is being listened to, take each number
+						* sequentially from the slice, and put it into the channel
+						*
+						 */
+						case nsChannel <- numbers[i]:
+							i += 1
+							/**
+							* if we've reached the last number and the channel still
+							* being listened to, start reading from the beginning of
+							* the slice again
+							 */
+							if i == len(numbers) {
+								i = 0
+							}
+							/**
+							* if we receive a message on the completed channel,
+							* we stop transmitting
+							 */
+						case <-cChannel:
+							cChannel <- true
+							return
+						}
+					}
+				}
+
+				numbersStation := make(chan int)
+				completeChannel := make(chan bool)
+
+				// execute broadcast in a separate thread
+				go broadcast2(numbersStation, completeChannel)
+
+				// get 100 numbers from the numbersStation channel
+				for i := 0; i < 100; i++ {
+					// delay for artistic effect
+					time.Sleep(100 * time.Millisecond)
+					// retrieve values from the channel
+					fmt.Printf("%d\t", <-numbersStation)
+				}
+				/**
+				* once we have received 100 numbers,
+				* send a message on completeChannel to tell it to stop
+				* transmitting
+				 */
+				completeChannel <- true
+				/**
+				* don't terminate the program until we receive
+				* a message on the completeChannel
+				*
+				* Discard the response.
+				 */
+				<-completeChannel
+
+				/**
+				* we only get to here if we received a
+				* message on completedChannel
+				 */
+				fmt.Println("\nTransmission Complete.")
+
+			}
+
+		}
+
 	}
+
+
 
 }
