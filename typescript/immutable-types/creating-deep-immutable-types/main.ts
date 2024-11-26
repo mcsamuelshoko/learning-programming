32bc55e9a86cff263af21cc838fec02959259232
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-11-26 07:46:58 +0200

    update main.ts - finished topic

--- typescript/immutable-types/creating-deep-immutable-types/main.ts
+++ typescript/immutable-types/creating-deep-immutable-types/main.ts
@@ -54,3 +54,40 @@ const labulu = deepFreeze({
 // labulu.scores.push(100);
 
 //* Creating a 'DeepImmutable' type
+type Person = {
+  name: string;
+  profile: {
+    level: number;
+  };
+  scores: number[];
+}
+
+type Immutable<T> = {
+  readonly[K in keyof T]: Immutable<T[K]>;
+};
+
+const labulushula: Immutable<Person> = deepFreeze({
+  name: "Bill",
+  profile: {
+    level: 1,
+  },
+  scores: [90, 65, 80],
+});
+//! raises error -> immutable types
+// labulushula.name = "Bob";
+// labulushula.profile.level = 2;
+// labulushula.scores.push(100);
+
+/**
+ *? Summary
+ * A const assertion is a convenient way of making an 
+ * object or array deeply immutable at compile-time.
+
+ * A function that recursively leverages Object-freeze can 
+ * make an object or array deeply immutable at runtime.
+
+ * A deep immutable mapped type can be created to 
+ * recursively add the readonly modifier to all the 
+ * properties in the type.
+ * 
+ */
