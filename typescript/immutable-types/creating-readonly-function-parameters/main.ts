4da85d8139d7788583aecdbd63584388d62fa992
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-11-26 07:00:26 +0200

    update main.ts - finished subtopic

--- typescript/immutable-types/creating-readonly-function-parameters/main.ts
+++ typescript/immutable-types/creating-readonly-function-parameters/main.ts
@@ -1,8 +1,42 @@
 //* Creating readonly array parameters
 
-function doubleScores(scores: number[]) {
-  scores.forEach((score, i) => (scores[i] = score * 2));
-  return scores;
+function doubleScores(scores: readonly number[]) {
+  return scores.map((score, i) => score * 2)
 }
 
 const billScores = [90, 65, 80];
+
+const doubledScores = doubleScores(billScores)
+console.log(billScores,doubledScores)
+
+//* Creating readonly Object Parameters
+
+type PersonScore = {
+  name: string;
+  score: number;
+};
+
+function doubleScore(person: Readonly<PersonScore>){
+  return {...person, score: person.score * 2}
+}
+
+const bill: PersonScore = {
+  name: "Bill",
+  score:  90,
+};
+
+const doubleBill = doubleScore(bill)
+console.log(bill,doubleBill)
+
+/**
+ *? Summary
+ * 
+ * Making function parameters immutable helps us write pure
+ * functions and avoid subtle bugs.
+
+ * Array function parameters can be made compile-time 
+ * immutable by using the readonly modifier. Object 
+ * parameters can be made shallow compile-time immutable 
+ * by using the Readonly utility type.
+ * 
+ */
