de35a48e6731a3a49c32b282c60528310c1c6a5a
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-11-26 06:04:52 +0200

    update main.ts - finished subtopic

--- typescript/immutable-types/using-object.freeze/main.ts
+++ typescript/immutable-types/using-object.freeze/main.ts
@@ -5,3 +5,32 @@
 
 let billScores = Object.freeze([90, 65, 80]);
 
+// billScores.push(100); //! error
+console.log(billScores);
+
+
+//* Using `Object.freeze` on objects
+
+let bill = Object.freeze({
+  name: "Bill",
+  age: 30,
+  scores: [90, 65, 80],
+  profile: {
+    level: 1,
+  },
+})
+
+// bill.age = 31 //! error
+bill.scores.push(100)
+bill.profile.level = 2;
+console.log(bill);
+
+/**
+ *? Summary
+ * 
+ * Object-freeze with the readonly modifier allows us to 
+ * create immutable arrays and objects at runtime and 
+ * compile-time. It is important to note that Object.freeze
+ * only performs a shallow freeze. 
+ * 
+ */
