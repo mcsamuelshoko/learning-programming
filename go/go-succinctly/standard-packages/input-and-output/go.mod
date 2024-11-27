4b7d499db54c128241c033f5ae14f175ad06c540
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-11-27 15:17:27 +0200

    build(deps): create go.mod and main.go - peaked into `input-output` package

---
+++ go/go-succinctly/standard-packages/input-and-output/go.mod
@@ -0,0 +1,3 @@
+module input-and-output
+
+go 1.21.4
---
+++ go/go-succinctly/standard-packages/input-and-output/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	// write a new file from a byte string
+	name := "test.txt"
+	txt := []byte("Not much in this file.")
+
+	if err := ioutil.WriteFile(name, txt, 0755); err != nil {
+		panic(err)
+	}
+
+	// Read the contents of a file into a []byte
+	results, err := ioutil.ReadFile(name)
+	if err != nil {
+		panic(err)
+	}
+	println(string(results))
+
+	// Or use os.Open(filename)
+	reader, err := os.Open(name)
+	if err != nil {
+		panic(err)
+	}
+
+	results, err = ioutil.ReadAll(reader)
+	if err != nil {
+		panic(err)
+	}
+	reader.Close()
+	println(string(results))
+}
