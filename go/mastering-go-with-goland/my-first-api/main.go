ab6e41f29fc231f6bc75f88776a50c9611239d82
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-12-01 21:46:36 +0200

    added a simple http server

--- go/mastering-go-with-goland/my-first-api/main.go
+++ go/mastering-go-with-goland/my-first-api/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /todo", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("Hello World"))
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
 }
