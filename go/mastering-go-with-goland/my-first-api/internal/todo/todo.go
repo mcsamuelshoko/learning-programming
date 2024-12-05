2b7a3cdec5d9d16ea939958c1efa7107e54419eb
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-12-05 07:26:05 +0200

    added state for todos for checking completion status

--- go/mastering-go-with-goland/my-first-api/internal/todo/todo.go
+++ go/mastering-go-with-goland/my-first-api/internal/todo/todo.go
@@ -4,26 +4,34 @@ import (
 	"errors"
 )
 
+type item struct {
+	Task  string
+	State string
+}
+
 type Service struct {
-	todos []string
+	todos []item
 }
 
 func NewService() *Service {
-	return &Service{todos: make([]string, 0)}
+	return &Service{todos: make([]item, 0)}
 }
 
 func (svc *Service) Add(todo string) error {
 	// checking if todos already exists
 	for _, t := range svc.todos {
-		if t == todo {
+		if t.Task == todo {
 			return errors.New("todo already exists")
 		}
 	}
+	todoItem := new(item)
+	todoItem.Task = todo
+	todoItem.State = "unfinished"
 	//adding new to todos
-	svc.todos = append(svc.todos, todo)
+	svc.todos = append(svc.todos, *todoItem)
 	return nil
 }
 
-func (svc *Service) GetAll() []string {
+func (svc *Service) GetAll() []item {
 	return svc.todos
 }
