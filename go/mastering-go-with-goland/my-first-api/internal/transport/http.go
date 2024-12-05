500bdf239c25d7f7e72c5dbfc08dce35450c0059
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-12-05 06:06:20 +0200

    Added internal/transport & refactored main.go

---
+++ go/mastering-go-with-goland/my-first-api/internal/transport/http.go
@@ -0,0 +1,64 @@
+package transport
+
+import (
+	"encoding/json"
+	"log"
+	"my-first-api/internal/todo"
+	"net/http"
+)
+
+type TodoItem struct {
+	Item string `json:"item"`
+}
+
+type Server struct {
+	mux *http.ServeMux
+}
+
+func NewServer(todoSvc *todo.Service) *Server {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("[route] GET:/")
+		_, err := w.Write([]byte("Hello Todo World!"))
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
+
+	mux.HandleFunc("GET /todo", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("[route] GET:/todo")
+
+		b, err := json.Marshal(todoSvc.GetAll())
+		if err != nil {
+			log.Fatal(err)
+		}
+		_, err = w.Write(b)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return
+	})
+
+	mux.HandleFunc("POST /todo", func(writer http.ResponseWriter, r *http.Request) {
+		log.Println("[route] POST:/todo")
+		var t TodoItem
+		err := json.NewDecoder(r.Body).Decode(&t)
+		if err != nil {
+			log.Println(err)
+			writer.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		todoSvc.Add(t.Item)
+		writer.WriteHeader(http.StatusCreated)
+		return
+
+	})
+	return &Server{mux: mux}
+}
+
+func (s *Server) Serve() error {
+	log.Println("starting server on: 8080")
+	return http.ListenAndServe(":8080", s.mux)
+
+}
--- go/mastering-go-with-goland/my-first-api/main.go
+++ go/mastering-go-with-goland/my-first-api/main.go
@@ -1,61 +1,16 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
-	"net/http"
+	"my-first-api/internal/todo"
+	"my-first-api/internal/transport"
 )
 
-var todos = make([]string, 0)
-
-type TodoItem struct {
-	Item string `json:"item"`
-}
-
 func main() {
-	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("[route] GET:/")
-		_, err := w.Write([]byte("Hello Todo World!"))
-		if err != nil {
-			log.Fatal(err)
-		}
-	})
-
-	mux.HandleFunc("GET /todo", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("[route] GET:/todo")
-
-		//err := json.NewEncoder(w).Encode(todos)
-		b, err := json.Marshal(todos)
-		if err != nil {
-			log.Fatal(err)
-		}
-		_, err = w.Write(b)
-		if err != nil {
-			log.Fatal(err)
-		}
-		return
-	})
-
-	mux.HandleFunc("POST /todo", func(writer http.ResponseWriter, r *http.Request) {
-		log.Println("[route] POST:/todo")
-		var t TodoItem
-		err := json.NewDecoder(r.Body).Decode(&t)
-		if err != nil {
-			log.Println(err)
-			writer.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		todos = append(todos, t.Item)
-		writer.WriteHeader(http.StatusCreated)
-		return
-
-	})
-
-	log.Println("starting server on: 8080")
-	if err := http.ListenAndServe(":8080", mux); err != nil {
+	svc := todo.NewService()
+	server := transport.NewServer(svc)
+	if err := server.Serve(); err != nil {
 		log.Fatal(err)
 	}
-
 }
