e8e314f7b2db47e7adcc526e442ebb52cf76be69
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-12-06 12:34:54 +0200

    data layer: - added database service

---
+++ go/mastering-go-with-goland/my-first-api/internal/db/db.go
@@ -0,0 +1,59 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+type DB struct {
+	pool *pgxpool.Pool
+}
+
+type Item struct {
+	Task   string
+	Status string
+}
+
+func new(user, password, host, dbname string, port int) (*DB, error) {
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%d/%s", user, password, host, port, dbname)
+	pool, err := pgxpool.Connect(context.Background(), connStr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to the db :%w", err)
+	}
+
+	if err := pool.Ping(context.Background()); err != nil {
+		return nil, fmt.Errorf("failed to ping the db: %w", err)
+	}
+	return &DB{pool: pool}, nil
+}
+
+func (db *DB) InsertItem(ctx context.Context, item Item) error {
+	query := `INSERT INTO todo_items(task, status) VALUES ($1, $2)`
+	_, err := db.pool.Exec(ctx, query, item.Task, item.Status)
+	return err
+}
+
+func (db *DB) GetAllItems(ctx context.Context) ([]Item, error) {
+	query := `SELECT task, status FROM todo_items`
+	rows, err := db.pool.Query(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var items []Item
+	for rows.Next() {
+		var item Item
+		err := rows.Scan(&item.Task, &item.Status)
+		if err != nil {
+			return nil, err
+		}
+		items = append(items, item)
+	}
+
+}
+
+func (db *DB) Close() {
+	db.pool.Close()
+}
