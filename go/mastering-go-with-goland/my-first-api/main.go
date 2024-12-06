54efcf13361237ffea5bfcb165a54674bd1065ed
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-12-06 15:38:08 +0200

    update: added db connectection on init

--- go/mastering-go-with-goland/my-first-api/main.go
+++ go/mastering-go-with-goland/my-first-api/main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"log"
+	"my-first-api/internal/db"
 	"my-first-api/internal/todo"
 	"my-first-api/internal/transport"
 )
 
 func main() {
-
-	svc := todo.NewService()
+	d, err := db.New("postgres", "example", "localhost", "postgres", 5432)
+	if err != nil {
+		log.Fatal(err)
+	}
+	svc := todo.NewService(d)
 	server := transport.NewServer(svc)
 	if err := server.Serve(); err != nil {
 		log.Fatal(err)
