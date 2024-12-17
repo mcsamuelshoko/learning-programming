72e58aaa036192da29b21e8994dfade9f99b939f
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-12-17 08:03:27 +0200

    update type_specifications.exs - finished topic on **`Type Specifications`**

--- elixir/elixir-succinctly/specifications/type_specifications.exs
+++ elixir/elixir-succinctly/specifications/type_specifications.exs
@@ -1,19 +1,36 @@
 defmodule Math do
-  @spec sum(integer, integer)::integer
-  def sum(a,b) do
+  @spec sum(integer, integer) :: integer
+  def sum(a, b) do
     a + b
   end
 
-  @spec div(integer, integer)::{:ok, integer}|{:error,String.t}
-  def div(a,b) do
+  @spec div(integer, integer) :: {:ok, integer} | {:error, String.t()}
+  def div(a, b) do
     if b == 0 do
       {:error, "cannot divide by 0"}
     else
-      {:ok,  trunc(a/b)}
+      {:ok, trunc(a / b)}
     end
   end
 end
 
-IO.puts Math.div(5,2)
+IO.puts(Math.div(5, 2))
 
-Math.div(5,0) |> IO.puts
+Math.div(5, 0) |> IO.puts()
+
+defmodule Customer do
+  @type entity_id() :: integer()
+  @type t :: %Customer{id: entry_id(), first_name: String.t(), last_name: String.t()}
+
+  defstruct id: 0, first_name: nil, last_name: nil
+end
+
+defmodule CustomerDao do
+  @type reason :: String.t()
+  @spec get_customer(Customer.entity_id()) :: {:ok, Customer} | {:error, reason}
+
+  def get_customer(id) do
+    # ...
+    IO.puts("[🔃]Getting Customer")
+  end
+end
