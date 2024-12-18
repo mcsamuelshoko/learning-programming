f231007801439ed6fbcccfb0e07b8100b559bf5c
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-12-18 10:22:12 +0200

    update async_maths.exs

--- elixir/elixir-succinctly/the-platform/async_maths.exs
+++ elixir/elixir-succinctly/the-platform/async_maths.exs
@@ -8,3 +8,19 @@ defmodule AsyncMath do
     start() # we do a recursive call to keep the process awake & in "waiting for message" mode
   end
 end
+
+pid = spawn(AsyncMath, :start,[])
+
+send(pid,{:sum, [5, 4], self()})
+send(pid, {:sum, [3, 9], self()})
+
+#iex> flush
+
+pid = spawn(AsyncMath, :start, [])
+send pid, {:sum, [5,6], self()}
+
+receive do
+  {:result, x} -> IO.puts x
+end
+
+# though this implementation is raw & primal, OTP is more suited in production
