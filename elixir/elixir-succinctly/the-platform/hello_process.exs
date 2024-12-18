f34393b749f46261630c5652934806de9d965bb0
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-12-18 06:20:41 +0200

    update hello_process.exs

--- elixir/elixir-succinctly/the-platform/hello_process.exs
+++ elixir/elixir-succinctly/the-platform/hello_process.exs
@@ -8,4 +8,21 @@ end
 
 HelloProcess.say("adam")
 
-spawn(HelloProcess, :say,["adam"])
+spawn(HelloProcess, :say,["Odom"])
+
+# Using a PID to inspect a process status using the Process module.
+pid = spawn(HelloProcess, :say,["Ada"])
+Process.alive?(pid)
+|> IO.puts
+
+# We need to do some calculation that we can spawn to another
+# process to keep the main process free
+
+defmodule AsyncMath do
+  def sum(a,b) do
+    a + b
+  end
+end
+
+pid = spawn(AsyncMath, :sum,[1,3])
+Process.alive?(pid) |> IO.puts
