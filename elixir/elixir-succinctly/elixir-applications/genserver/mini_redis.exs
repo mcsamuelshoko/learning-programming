00f9712e38189d11e54f600923f51f414242a772
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-12-24 11:54:12 +0200

    update mini_redis.exs - finished genservers subtopic

--- elixir/elixir-succinctly/elixir-applications/genserver/mini_redis.exs
+++ elixir/elixir-succinctly/elixir-applications/genserver/mini_redis.exs
@@ -1,8 +1,38 @@
+# simple implementation
 defmodule MiniRedis do
   use GenServer
 
   def init(_) do
     {:ok, %{}}
+  end
+
+  def handle_call({:set, key, value}, _from, state) do
+    {:reply, :ok, Map.merge(state, %{key => value})}
+  end
+
+  def handle_call({:get, key}, _from, state) do
+    {:reply, Map.fetch(state, key), state}
+  end
+end
+
+# modifying the first implementation to add the public interface
+defmodule MiniRedisINT do
+  use GenServer
+
+  def init(_) do
+    {:ok, %{}}
+  end
+
+  def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, [], opts)
+  end
+
+  def set(key, value) do
+    GenServer.call(__MODULE__, {:set, key, value})
+  end
+
+  def get(key) do
+    GenServer.call(__MODULE__, {:get, key})
   end
 
   def handle_call({:set, key, value}, _from, state) do
