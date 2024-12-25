7688daa941ab0265d3a273099dc1f3bda6d568da
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-12-25 10:04:54 +0200

    chore: created rover app create 7 files

---
+++ elixir/elixir-succinctly/sample-application/rover/lib/rover.ex
@@ -0,0 +1,18 @@
+defmodule Rover do
+  @moduledoc """
+  Documentation for `Rover`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Rover.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
