fc5f040e2dc2691ddc77887929b4194f54549553
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-12-26 17:37:13 +0200

    update rover.ex - added genserver

--- elixir/elixir-succinctly/sample-application/rovex/lib/rover.ex
+++ elixir/elixir-succinctly/sample-application/rovex/lib/rover.ex
@@ -1,3 +1,12 @@
 defmodule Rover do
+  use GenServer
+
+  def start_link({x, y, d, name}) do
+    GenServer.start_link(__MODULE__, {x, y, d, name}, name: String.to_atom(name))
+  end
+
+  def init({x, y, d, name}) do
+    {:ok, %{}}
+  end
 
 end
