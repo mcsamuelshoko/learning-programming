2df0772d5d396b52b73ad0d2b88e11058ec616b7
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-12-26 19:36:28 +0200

    update rover.ex - added a state struct

--- elixir/elixir-succinctly/sample-application/rovex/lib/rover.ex
+++ elixir/elixir-succinctly/sample-application/rovex/lib/rover.ex
@@ -1,12 +1,22 @@
 defmodule Rover do
   use GenServer
 
+  defstruct [:x, :y, :direction, :name]
+
   def start_link({x, y, d, name}) do
     GenServer.start_link(__MODULE__, {x, y, d, name}, name: String.to_atom(name))
   end
 
   def init({x, y, d, name}) do
-    {:ok, %{}}
+    {:ok, %Rover{x: x, y: y, direction: d, name: name}}
+  end
+
+  def get_state(name) do
+    GenServer.call(String.to_atom(name), :get_state)
+  end
+
+  def handle_call(:get_state, _from, state) do
+    {:reply, {:ok, {state.x, state.y, state.direction}}, state}
   end
 
 end
