b541c606c9c4bd340ef79783c0540af36dab69ef
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-12-28 15:32:29 +0200

    update: implemented method overriding

--- csharp/oop-in-csharp-succinctly/introduction-to-oop/Person.cs
+++ csharp/oop-in-csharp-succinctly/introduction-to-oop/Person.cs
@@ -1,14 +1,65 @@
+using System;
+
+// Introduction to OOP
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+
+    Person personObject = new Person();
+    personObject.FirstName = "Sander";
+    personObject.LastName = "Rossel";
+    string fullName = personObject.GetFullName();
+
+
+    // Inheritance
+
+    public class Employee: Person
+    {
+        public decimal Salary { get; set; }
+    }
+
+    Employee employee = new Employee();
+    employee.FirstName = "Sander";
+    employee.LastName = "Rossel";
+    string fullName = employee.GetFullName();
+    employee.Salary = 1000000;// I wish! :-) [soon though ;-)]
+
+}
+
+// Method Overriding
 public class Person
 {
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public string GetFullName()
+    public virtual string GetFullName()
     {
         return FirstName + " " + LastName;
     }
 }
 
-Person personObject = new Person();
-personObject.FirstName = "Sander";
-personObject.LastName = "Rossel";
-string fullName = personObject.GetFullName();
+public class Employee : Person
+{
+    public decimal Salary { get; set; }
+    public override string GetFullName()
+    {
+        string originalValue = base.GetFullName();
+        return LastName + ", " + FirstName;
+    }
+}
+
+//QSTN: what will the following code print ?
+//ANS:  Shoko, Mc Samuel
+Person person = new Employee();
+person.FirstName = "Mc Samuel";
+person.LastName = "Shoko";
+string fullName = person.GetFullName();
+Console.WriteLine(fullName);
+// press any key to quit.
+Console.ReadKey();
