f8415b62525438284d960edea70ef564d96f6318
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-12-28 13:10:24 +0200

    update rover.ex - added directional functions to implement the movements for the rover

--- elixir/elixir-succinctly/sample-application/rovex/lib/rover.ex
+++ elixir/elixir-succinctly/sample-application/rovex/lib/rover.ex
@@ -19,4 +19,21 @@ defmodule Rover do
     {:reply, {:ok, {state.x, state.y, state.direction, state.name}}, state}
   end
 
+  @world_width 100
+  @world_height 100
+
+  def go_forward(name) do
+    GenServer.cast(RegistryHelper.create_key(name), :go_forward)
+  end
+
+  def handle_cast(:go_forward, state) do
+    new_state = case state.direction do
+      :N -> %Rover{ state | x: state.x, y: mod(state.y + 1, @world_height) }
+      :S -> %Rover{ state | x: state.x, y: mod(state.y - 1, @world_height) }
+      :E -> %Rover{ state | x: mod(state.x + 1, @world_width), y: state.y }
+      :W -> %Rover{ state | x: mod(state.x - 1, @world_width), y: state.y }
+    end
+    {:noreply, new_state}
+  end
+
 end
