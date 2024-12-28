a58419c5025b669dfb027c092498f58eae0a9b2e
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2024-12-28 12:35:07 +0200

    test: update rover_test.exs - added test for forward movement

--- elixir/elixir-succinctly/sample-application/rovex/test/rover_test.exs
+++ elixir/elixir-succinctly/sample-application/rovex/test/rover_test.exs
@@ -7,4 +7,11 @@ defmodule RoverTest do
     {:ok, state} = Rover.get_state("rover0")
     assert state == {9, 9, :N, "rover0"}
   end
+
+  test "handle_cast :go_forward should return updated state" do
+    {:noreply, state} = Rover.handle_cast(:go_forward, %Rover{x: 1, y: 3, direction: :N})
+    assert state.x == 1
+    assert state.y == 4
+    assert state.direction == :N
+  end
 end
