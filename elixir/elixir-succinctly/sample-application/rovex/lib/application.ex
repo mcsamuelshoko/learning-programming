849eb66c9eb6b848d73da422758d791422106ac3
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2025-01-23 23:19:28 +0200

    feat: create application.ex, added entry point for app

---
+++ elixir/elixir-succinctly/sample-application/rovex/lib/application.ex
@@ -0,0 +1,14 @@
+defmodule Rover.Application do
+  use Application
+
+  def start(_type, _args) do
+
+    children = [
+      Supervisor.child_spec({Registry, [keys: :unique, name: Rover.Registry]}, id: :rover_registry),
+      Supervisor.child_spec({RoverSupervisor, [], id: RoverSupervisor}),
+    ]
+
+    opts = [strategy: :one_for_one, name: Application.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
