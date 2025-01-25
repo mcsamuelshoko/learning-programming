d06b00b66f48d2ec14acb9de989dd40788caac1c
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2025-01-25 11:27:00 +0200

    update web_router.ex, added post request

--- elixir/elixir-succinctly/sample-application/rovex/lib/web_router.ex
+++ elixir/elixir-succinctly/sample-application/rovex/lib/web_router.ex
@@ -12,4 +12,17 @@ defmodule Rover.Web.Router do
   match(_) do
     send_resp(conn, 404, "")
   end
+
+  post "/rover" do
+    rover_name = conn.body_params["name"]
+    x = conn.body_params["x"]
+    y = conn.body_params["y"]
+    d = String.to_atom(conn.body_params["d"])
+
+    case RoverSupervisor.create_rover(name, x, y, d) do
+      {:ok, _} -> send_resp(conn, 201, encode(%{message: "created rover #{rover_name}"}))
+      {:error, {:already_started, _}} -> send_resp(conn, 400, encode(%{message: "rover already exists"}))
+      _ -> send_resp(conn, 500, encode(%{message: "generic error"}))
+    end
+  end
 end
