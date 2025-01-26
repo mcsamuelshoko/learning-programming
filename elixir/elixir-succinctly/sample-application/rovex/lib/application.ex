84e154040547a1a6f5b2a615be61524fc847f215
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2025-01-27 00:40:06 +0200

    update application.ex, added websocket communication channel

--- elixir/elixir-succinctly/sample-application/rovex/lib/application.ex
+++ elixir/elixir-succinctly/sample-application/rovex/lib/application.ex
@@ -9,7 +9,19 @@ defmodule Rover.Application do
       Plug.Adapters.Cowboy.child_spec(:http, Rover.Web.Router, [], port: 3000),
       Supervisor.child_spec({RoverSupervisor, []}, id: RoverSupervisor),
       Supervisor.child_spec({WorldMap, []}, []),
+      Plug.Adapters.Cowboy.child_spec(:http, Rover.Web.Router, [], port: 3000, dispatch: dispatch()),
     ]
+
+    defp dispatch do
+      [
+        {:_,
+          [
+            {"/ws", Rover.Web.WsServer, []},
+            {:_, Plug.Adapters.Cowboy.Handler, {}}
+          ]
+        }
+      ]
+    end
 
     opts = [strategy: :one_for_one, name: Application.Supervisor]
     Supervisor.start_link(children, opts)
