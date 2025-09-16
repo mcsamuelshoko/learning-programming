8965cd133c906d7fb31e17e08b039b9ae04adadb
Mc Samuel Shoko <66551316+mcsamuelshoko@users.noreply.github.com>
2025-09-16 21:55:16 +0200

    feat: create p.rb

---
+++ ruby/strings-numbers-classes/p.rb
@@ -0,0 +1,13 @@
+class Treasure
+  def initialize( aName, aDescription )
+    @name = aName
+    @description = aDescription
+  end
+  def to_s # override default to_s method
+    "The #{@name} Treasure is #{@description}\n"
+  end
+end
+
+a = "hello"
+b = 123
+c = Treasure.new( "ring", "a glittery gold thing" )
