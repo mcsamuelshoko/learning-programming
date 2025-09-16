bfcea3fc1b8cb47e3a95ff410405e53ec8e2e0f0
Mc Samuel Shoko <66551316+mcsamuelshoko@users.noreply.github.com>
2025-09-16 21:16:16 +0200

    update:  created object, named & asked it to talk,6dogs.rb

--- ruby/strings-numbers-classes/6dogs.rb
+++ ruby/strings-numbers-classes/6dogs.rb
@@ -19,5 +19,12 @@ yourdog = Dog.new
 
 # Call the method to give them names
 
-mydog.set_name( "Fido" )
+# mydog.set_name( "Fido" )
 yourdog.set_name( "Bonzo" )
+
+# create dog, name it, display its name, and ask it to talk:
+
+mydog = Dog.new
+mydog.set_name( 'Fido' )
+puts(mydog.get_name)
+puts(mydog.talk)
