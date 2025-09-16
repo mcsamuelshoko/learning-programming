c5fd306b1070b3dedeb0b861b1d5d3511a9681eb
Mc Samuel Shoko <66551316+mcsamuelshoko@users.noreply.github.com>
2025-09-16 06:12:31 +0200

    update 4caltax.rb

--- ruby/strings-numbers-classes/4caltax.rb
+++ ruby/strings-numbers-classes/4caltax.rb
@@ -2,3 +2,17 @@ subtotal=100
 taxrate = 0.175
 tax = subtotal * taxrate
 puts "Tax on $#{subtotal} is $#{tax}, so grand total is $#{subtotal+tax}"
+
+
+############################
+# Prompting user to rather use subtotal
+############################
+
+puts ""
+
+taxrate = 0.175
+print "Enter price (ex tax): "
+s = gets
+subtotal = s.to_f
+tax = subtotal * taxrate
+puts "Tax on $#{subtotal} i $#{tax}, so grand total is $#{subtotal+tax}"
