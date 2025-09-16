b4da4af3ca728c9083cbbd6e1619732134a3b186
Mc Samuel Shoko <66551316+mcsamuelshoko@users.noreply.github.com>
2025-09-16 06:48:27 +0200

    update variables.rb

--- ruby/strings-numbers-classes/variables.rb
+++ ruby/strings-numbers-classes/variables.rb
@@ -2,14 +2,22 @@ localvar = "hello"
 $globalvar = "goodbye"
 
 def amethod
+  puts "[amethod]"
   localvar = 10
   puts( localvar )
   puts( $globalvar )
 end
 
 def anotherMethod
+  puts "[anotherMethod]"
   localvar = 500
   $globalvar = "bonjour"
   puts( localvar )
   puts( $globalvar )
 end
+
+# calling the methods
+amethod
+anotherMethod
+amethod
+puts( localvar )
